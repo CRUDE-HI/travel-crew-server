b7254afe6ea14022de9e3a495772054e2507f8fa
yiseul_notebook <kimdew402@gmail.com>
2023-10-03 21:10:41 +0900

    feat : 동행 게시판 memberList DTO 추가

---
+++ src/main/java/com/crude/travelcrew/domain/board/dto/MemberListReq.java
@@ -0,0 +1,13 @@
+package com.crude.travelcrew.domain.board.dto;
+
+public class MemberListReq {
+	private Long memberId;
+
+	public Long getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+}
