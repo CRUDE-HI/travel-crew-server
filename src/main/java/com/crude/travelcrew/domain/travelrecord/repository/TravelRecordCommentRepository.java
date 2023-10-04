e0173770c285350931d88182e0ea65efaccae727
grace <grace994084@gmail.com>
2023-10-04 18:42:30 +0900

    feat : 여행 기록 삭제 시 댓글 일괄 삭제

--- src/main/java/com/crude/travelcrew/domain/travelrecord/repository/TravelRecordCommentRepository.java
+++ src/main/java/com/crude/travelcrew/domain/travelrecord/repository/TravelRecordCommentRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.crude.travelcrew.domain.travelrecord.model.entity.TravelRecord;
 import com.crude.travelcrew.domain.travelrecord.model.entity.TravelRecordComment;
+import com.crude.travelcrew.domain.travelrecord.repository.custom.CustomTravelRecordCommentRepository;
 
-public interface TravelRecordCommentRepository extends JpaRepository<TravelRecordComment, Long> {
+public interface TravelRecordCommentRepository extends JpaRepository<TravelRecordComment, Long>,
+	CustomTravelRecordCommentRepository {
 
 	Optional<TravelRecordComment> findByTravelRecordAndAndId(TravelRecord travelRecord, Long travelRecordCommentId);
 }
