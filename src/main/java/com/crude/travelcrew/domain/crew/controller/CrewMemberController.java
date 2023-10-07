db41f73e2ca0741f34105892c4c32b763e06fb5b
grace <grace994084@gmail.com>
2023-10-07 23:08:44 +0900

    feat : 동행 신청한 회원 목록 조회 구현

--- src/main/java/com/crude/travelcrew/domain/crew/controller/CrewMemberController.java
+++ src/main/java/com/crude/travelcrew/domain/crew/controller/CrewMemberController.java
@@ -1,12 +1,14 @@
 package com.crude.travelcrew.domain.crew.controller;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crude.travelcrew.domain.crew.model.dto.ApplyForCrewReq;
+import com.crude.travelcrew.domain.crew.model.dto.CrewMemberRes;
 import com.crude.travelcrew.domain.crew.service.CrewMemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -38,6 +41,13 @@ public class CrewMemberController {
 	public ResponseEntity<Map<String, String>> cancelCrewMember(@PathVariable Long crewId, Principal principal) {
 
 		Map<String, String> response = crewMemberService.cancelCrewMember(crewId, principal.getName());
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/{crewId}/list")
+	public ResponseEntity<List<CrewMemberRes>> getCrewMemberList(@PathVariable Long crewId) {
+
+		List<CrewMemberRes> response = crewMemberService.getCrewMemberList(crewId);
 		return ResponseEntity.ok(response);
 	}
 }
--- src/main/java/com/crude/travelcrew/domain/crew/service/CrewMemberService.java
+++ src/main/java/com/crude/travelcrew/domain/crew/service/CrewMemberService.java
@@ -1,8 +1,10 @@
 package com.crude.travelcrew.domain.crew.service;
 
+import java.util.List;
 import java.util.Map;
 
 import com.crude.travelcrew.domain.crew.model.dto.ApplyForCrewReq;
+import com.crude.travelcrew.domain.crew.model.dto.CrewMemberRes;
 
 public interface CrewMemberService {
 
@@ -15,4 +17,9 @@ public interface CrewMemberService {
 	 * 동행 신청 취소
 	 */
 	Map<String, String> cancelCrewMember(Long crewId, String email);
+
+	/**
+	 * 동행 신청한 회원 목록 조회
+	 */
+	List<CrewMemberRes> getCrewMemberList(Long crewId);
 }
