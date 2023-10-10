a57053a6c5ae2cb57ea2c0e8aa8f9441e1b04a96
grace <grace994084@gmail.com>
2023-10-10 20:06:14 +0900

    fix : import 및 패키지 경로 변경

--- src/main/java/com/crude/travelcrew/domain/email/model/EmailAuthCode.java
+++ src/main/java/com/crude/travelcrew/domain/email/model/EmailAuthCode.java
@@ -2,10 +2,11 @@ package com.crude.travelcrew.domain.email.model;
 
 import lombok.Builder;
 import lombok.Getter;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
-import javax.persistence.Id;
 import java.util.concurrent.TimeUnit;
 
 @Getter
