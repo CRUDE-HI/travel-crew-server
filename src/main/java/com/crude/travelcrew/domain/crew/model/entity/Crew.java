962bbc521adb8314921d55fd271593d6b358de56
Kimjungwoo <rara980928@gmail.com>
2023-10-10 18:04:26 +0900

    [feat] fetch 재수정

--- src/main/java/com/crude/travelcrew/domain/crew/model/entity/Crew.java
+++ src/main/java/com/crude/travelcrew/domain/crew/model/entity/Crew.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -46,7 +47,7 @@ public class Crew extends BaseTime {
 	@Column(nullable = false, length = 30)
 	private String title;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id", updatable = false)
 	private Member member;
 
