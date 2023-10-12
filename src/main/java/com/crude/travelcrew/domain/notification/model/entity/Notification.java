777fed01988fb249319501ae74e9f3b8e03b2899
grace <grace994084@gmail.com>
2023-10-12 19:22:15 +0900

    feat : 알림 entity 생성

---
+++ src/main/java/com/crude/travelcrew/domain/notification/model/entity/Notification.java
@@ -0,0 +1,50 @@
+package com.crude.travelcrew.domain.notification.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.crude.travelcrew.domain.member.model.entity.Member;
+import com.crude.travelcrew.domain.notification.model.constants.NotificationType;
+import com.crude.travelcrew.global.entity.BaseTime;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseTime {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "notification_id")
+	private Long id;
+
+	@Column(nullable = false)
+	private String content;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@Column(nullable = false)
+	private boolean isRead;
+
+	private String relatedUrl;
+
+	@Enumerated(EnumType.STRING)
+	private NotificationType notificationType;
+}
